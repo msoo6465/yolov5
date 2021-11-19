c005bfc2695f0eea37e05b8a67e3424328783fd1
msoo6465 <msoo6465@gmail.com>
2021-11-19 11:45:03 +0900

    CHANGE BOX LINE

--- utils/plots.py
+++ utils/plots.py
@@ -86,30 +86,30 @@ class Annotator:
             if not class_ == 'car':
                 xyxy_ = [int(i.tolist()) for i in box]
                 self.im = mosaic_area(self.im,*xyxy_,ratio=0.1)
-
-        # Add one xyxy box to image with label
-        if self.pil or not is_ascii(label):
-            self.draw.rectangle(box, width=self.lw, outline=color)  # box
-            if label:
-                w, h = self.font.getsize(label)  # text width, height
-                outside = box[1] - h >= 0  # label fits outside box
-                self.draw.rectangle([box[0],
-                                     box[1] - h if outside else box[1],
-                                     box[0] + w + 1,
-                                     box[1] + 1 if outside else box[1] + h + 1], fill=color)
-                # self.draw.text((box[0], box[1]), label, fill=txt_color, font=self.font, anchor='ls')  # for PIL>8.0
-                self.draw.text((box[0], box[1] - h if outside else box[1]), label, fill=txt_color, font=self.font)
-        else:  # cv2
-            p1, p2 = (int(box[0]), int(box[1])), (int(box[2]), int(box[3]))
-            cv2.rectangle(self.im, p1, p2, color, thickness=self.lw, lineType=cv2.LINE_AA)
-            if label:
-                tf = max(self.lw - 1, 1)  # font thickness
-                w, h = cv2.getTextSize(label, 0, fontScale=self.lw / 3, thickness=tf)[0]  # text width, height
-                outside = p1[1] - h - 3 >= 0  # label fits outside box
-                p2 = p1[0] + w, p1[1] - h - 3 if outside else p1[1] + h + 3
-                cv2.rectangle(self.im, p1, p2, color, -1, cv2.LINE_AA)  # filled
-                cv2.putText(self.im, label, (p1[0], p1[1] - 2 if outside else p1[1] + h + 2), 0, self.lw / 3, txt_color,
-                            thickness=tf, lineType=cv2.LINE_AA)
+        if is_box:
+            # Add one xyxy box to image with label
+            if self.pil or not is_ascii(label):
+                self.draw.rectangle(box, width=self.lw, outline=color)  # box
+                if label:
+                    w, h = self.font.getsize(label)  # text width, height
+                    outside = box[1] - h >= 0  # label fits outside box
+                    self.draw.rectangle([box[0],
+                                         box[1] - h if outside else box[1],
+                                         box[0] + w + 1,
+                                         box[1] + 1 if outside else box[1] + h + 1], fill=color)
+                    # self.draw.text((box[0], box[1]), label, fill=txt_color, font=self.font, anchor='ls')  # for PIL>8.0
+                    self.draw.text((box[0], box[1] - h if outside else box[1]), label, fill=txt_color, font=self.font)
+            else:  # cv2
+                p1, p2 = (int(box[0]), int(box[1])), (int(box[2]), int(box[3]))
+                cv2.rectangle(self.im, p1, p2, color, thickness=self.lw, lineType=cv2.LINE_AA)
+                if label:
+                    tf = max(self.lw - 1, 1)  # font thickness
+                    w, h = cv2.getTextSize(label, 0, fontScale=self.lw / 3, thickness=tf)[0]  # text width, height
+                    outside = p1[1] - h - 3 >= 0  # label fits outside box
+                    p2 = p1[0] + w, p1[1] - h - 3 if outside else p1[1] + h + 3
+                    cv2.rectangle(self.im, p1, p2, color, -1, cv2.LINE_AA)  # filled
+                    cv2.putText(self.im, label, (p1[0], p1[1] - 2 if outside else p1[1] + h + 2), 0, self.lw / 3, txt_color,
+                                thickness=tf, lineType=cv2.LINE_AA)
 
     def rectangle(self, xy, fill=None, outline=None, width=1):
         # Add rectangle to image (PIL-only)
